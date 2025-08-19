fd8a16411a16e20b61f4c39bf9c337157a6ca111
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-19 22:40:52 +0530

    background changer

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-
 function App() {
-  const [color, setColor] = useState("black");
-
+  const [color,setColor] = useState("black")
   return (
-    <div style={{ backgroundColor: color, height: "50vh", width:"50vw", alignItems:"center" }}>
-      <div className="flex gap-4 p-4 justify-center">
-        <button onClick={() => setColor("red")}>Red</button>
-        <button onClick={() => setColor("green")}>Green</button>
-        <button onClick={() => setColor("yellow")}>Yellow</button>
-        <button onClick={() => setColor("blue")}>Blue</button>
+    <>
+    <div className="w-full h-screen items-center justify-center" style={{backgroundColor:color}}>
+      <div className="flex flex-1 gap-3">
+        <button onClick={()=>{setColor("red")}}>red</button>
+        <button onClick={()=>{setColor("blue")}}>blue</button>
+        <button onClick={()=>{setColor("yellow")}}>yellow</button>
+        <button onClick={()=>{setColor("blue")}}>blue</button>
       </div>
     </div>
+    </>
   );
 }
-
-export default App;
+export default App
